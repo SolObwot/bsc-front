7bdf92b4b3f017a6b56c307f670098ad9dbcc016
SolObwot <solobwot@gmail.com>
2025-06-17 17:46:22 +0300

    fix: Adjust padding in InfoBanner and simplify reminder text

--- src/components/balancescorecard/InfoBanner.jsx
+++ src/components/balancescorecard/InfoBanner.jsx
@@ -3,17 +3,14 @@ import { InformationCircleIcon} from '@heroicons/react/20/solid';
 
 const InfoBanner = () => {
   return (
-    <div className="rounded-md bg-teal-700 p-4 m-4 text-white">
+    <div className="rounded-md bg-teal-700 p-2 m-4 text-white">
       <div className="flex">
         <div className="shrink-0">
           <InformationCircleIcon aria-hidden="true" className="size-5" />
         </div>
         <div className="ml-3 flex-1 md:flex md:justify-between">
           <p className="text-sm">
-            This is a friendly reminder that all you need to do on this tab is to set your agreement. If you are ready to start your appraisals,{' '}
-            <a href="#" className="font-medium whitespace-nowrap underline hover:text-blue-600">
-              click here
-            </a>. To submit your agreement for review,{' '}
+            This is a friendly reminder that all you need to do on this tab is to set your agreement. To submit your agreement for review,{' '}
             <a href="#" className="font-medium whitespace-nowrap underline hover:text-blue-600">
               click here
             </a>.
